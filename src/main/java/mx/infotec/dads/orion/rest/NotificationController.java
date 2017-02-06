fafbaf73512a216a1600899743b831b9e24b5e78
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-02-06 14:33:17 -0600

    updating NotificationController
    Updating notificationController in order to use only one controller

--- src/main/java/mx/infotec/dads/orion/rest/NotificationController.java
+++ src/main/java/mx/infotec/dads/orion/rest/NotificationController.java
@@ -28,12 +28,15 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import mx.infotec.dads.orion.model.Notification;
 import mx.infotec.dads.orion.repository.NotificationRepository;
@@ -66,6 +69,15 @@ public class NotificationController {
         } else {
             return new ResponseEntity<List<Notification>>(dataStoreList, HttpStatus.OK);
         }
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createNotification(@RequestBody String text, UriComponentsBuilder ucBuilder) {
+        Notification notification = repository.save(new Notification(text));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/{id}").buildAndExpand(notification.getId()).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
