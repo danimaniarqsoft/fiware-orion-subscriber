225d7149c0af0073be5d6cf77adebc03f03c2829
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-03-06 13:02:33 -0600

    udpating values
    updating ip

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute','ngResource']).constant("APP_URL", 'http://localhost:8080');
+var app = angular.module('app', ['ngRoute','ngResource']).constant("APP_URL", 'http://207.249.127.180:8080');
 app.config(function($routeProvider){
     $routeProvider
         .when('/dashboard',{
